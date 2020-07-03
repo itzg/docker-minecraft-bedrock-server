ba5ce2d4a317cb4b0136c43032b8bc0a2ce1c375
Geoff Bourne <itzgeoff@gmail.com>
2020-07-03 10:17:38 -0500

    Added github actions with tests

---
+++ tests/test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+runTest() {
+  composeArgs="-f docker-compose.$1.test.yml -p $1"
+  failed=false
+  docker-compose ${composeArgs} run sut || failed=true
+  $failed && docker-compose ${composeArgs} logs
+  docker-compose ${composeArgs} down -v
+  if $failed; then
+    echo "Test failed using compose project $1: $2"
+    exit 1
+  fi
+}
+
+cd $(dirname $0)
+
+runTest defaults "using defaults"
+
+docker volume create mc-shared
+trap "docker volume rm mc-shared" EXIT
+
+export BDS_VERSION=1.14
+runTest versioned "with VERSION=1.14"
+export BDS_VERSION=1.16
+runTest versioned "upgrading to VERSION=1.16"
