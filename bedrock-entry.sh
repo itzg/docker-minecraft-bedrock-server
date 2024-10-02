14db82d702efa796da25ed977ec2db0d4eabcb6c
teedog <32413765+teedog@users.noreply.github.com>
2024-10-02 12:45:47 +0200

    Added user-agent parameter to enable download from minecraft.net.

--- bedrock-entry.sh
+++ bedrock-entry.sh
@@ -111,7 +111,7 @@ if [[ ! -f "bedrock_server-${VERSION}" ]]; then
   TMP_ZIP="$TMP_DIR/$(basename "${DOWNLOAD_URL}")"
 
   echo "Downloading Bedrock server version ${VERSION} ..."
-  if ! curl "${curlArgs[@]}" -o "${TMP_ZIP}" -A "itzg/minecraft-bedrock-server" -fsSL "${DOWNLOAD_URL}"; then
+  if ! curl "${curlArgs[@]}" -H "user-agent: itzg/docker-minecraft-bedrock-server" -o "${TMP_ZIP}" -A "itzg/minecraft-bedrock-server" -fsSL "${DOWNLOAD_URL}"; then
     echo "ERROR failed to download from ${DOWNLOAD_URL}"
     echo "      Double check that the given VERSION is valid"
     exit 2
